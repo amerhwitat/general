0b149bc0af67a0d4bd38f99eb587c180d0cfaf7b
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-12 17:39:11 +0300

    Harden macOS application shell

--- Apple-Implementations/Chimera3D4D/apple/objc/CH3D4DAppDelegate.m
+++ Apple-Implementations/Chimera3D4D/apple/objc/CH3D4DAppDelegate.m
@@ -1,10 +1,16 @@
 #import "CH3D4DAppDelegate.h"
 #import "CH3D4DViewport.h"
+
 @implementation CH3D4DAppDelegate
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-    NSWindow *window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0,0,1280,800) styleMask:(NSWindowStyleMaskTitled|NSWindowStyleMaskClosable|NSWindowStyleMaskResizable) backing:NSBackingStoreBuffered defer:NO];
-    window.title = @"Chimera 3D/4D Studio";
-    window.contentView = [[CH3D4DViewport alloc] initWithFrame:window.contentView.bounds];
-    [window makeKeyAndOrderFront:nil];
+    (void)notification;
+    self.window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 1280, 800)
+                                              styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable)
+                                                backing:NSBackingStoreBuffered
+                                                  defer:NO];
+    self.window.title = @"Chimera 3D/4D Studio";
+    self.window.contentView = [[CH3D4DViewport alloc] initWithFrame:self.window.contentView.bounds];
+    [self.window center];
+    [self.window makeKeyAndOrderFront:nil];
 }
 @end
