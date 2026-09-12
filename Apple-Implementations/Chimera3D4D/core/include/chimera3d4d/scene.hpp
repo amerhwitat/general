7bc26cd2426898ca15149d69c9b88b73a134431a
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-12 17:42:31 +0300

    Add time-sampled scene evaluation

--- Apple-Implementations/Chimera3D4D/core/include/chimera3d4d/scene.hpp
+++ Apple-Implementations/Chimera3D4D/core/include/chimera3d4d/scene.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "geometry.hpp"
+#include <array>
 #include <cstdint>
 #include <string>
 #include <unordered_map>
@@ -7,15 +8,23 @@
 
 namespace chimera3d4d {
 struct TimeSample { double time{}; Transform transform{}; };
-struct SceneObject { std::uint64_t id{}; std::string name; Mesh mesh; std::vector<TimeSample> samples; std::array<float,128> state128{}; };
+struct SceneObject {
+  std::uint64_t id{};
+  std::string name;
+  Mesh mesh;
+  std::vector<TimeSample> samples;
+  std::array<float,128> state128{};
+};
+
 class Scene {
 public:
   std::uint64_t addObject(SceneObject object);
   const SceneObject* find(std::uint64_t id) const;
+  bool evaluate(std::uint64_t id, double time, Transform& out) const;
   bool validate() const;
   std::size_t size() const noexcept { return objects_.size(); }
 private:
   std::uint64_t nextId_{1};
   std::unordered_map<std::uint64_t, SceneObject> objects_;
 };
-}
+} // namespace chimera3d4d
